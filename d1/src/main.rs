1df7d6043c293574343078bef8db89415cda4fe4
xelox <alex@gorodoro.dev>
2023-12-01 13:18:18 +0200

    d1 p1

--- d1/src/main.rs
+++ d1/src/main.rs
@@ -1,3 +1,28 @@
+use std::{env, fs};
+
 fn main() {
-    println!("Hello, world!");
+    let path = env::args().nth(1).unwrap();
+    let input = fs::read_to_string(path).unwrap();
+    let mut sum: u32 = 0;
+
+    for line in input.lines() {
+        let bytes = line.as_bytes();
+        let mut num = 0;
+        for c in bytes {
+            let u = c - '0' as u8;
+            if u <= 9 { 
+                num = u * 10;
+                break;
+            }
+        }
+        for c in bytes.iter().rev() {
+            let u = c - '0' as u8;
+            if u <= 9 {
+                num += u;
+                break;
+            }
+        }
+        sum += num as u32;
+    }
+    println!("Part 1 answer: {sum}");
 }
