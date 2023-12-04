33f050d819fb59c7eb97dce21d0266608c396345
xelox <alex@gorodoro.dev>
2023-12-04 19:19:37 +0200

    d4 p2 even more optimized code. kind of obsessive at this point.

--- d4/src/main.rs
+++ d4/src/main.rs
@@ -1,28 +1,19 @@
-use std::{env::args, fs};
-
-struct ScratchCards {
-    instances: usize,
-    wins_count: usize,
-}
-
-impl ScratchCards {
-    fn new(wins_count: usize) -> ScratchCards {
-        ScratchCards {
-            instances: 1,
-            wins_count
-        }
-    }
-}
+use std::{env::args, fs, u32};
 
 fn main() {
     let path = args().nth(1).unwrap();
-    let input = fs::read_to_string(path).unwrap();
+
+    let input = fs::read_to_string(path)
+        .unwrap();
+
+    let lines: Vec<&str> = input.lines().collect();
 
     let mut p1_answer: u32 = 0;
 
-    let mut scratch_cards: Vec<ScratchCards> = Vec::new();
+    let cards_count = lines.len();
+    let mut scratch_cards = vec![1; cards_count];
      
-    for line in input.lines() {
+    for (i, line) in lines.iter().enumerate() {
         let numbers_part = line.split(": ").nth(1).unwrap();
         let mut vert_split = numbers_part.split(" | ");
 
@@ -50,20 +41,13 @@ fn main() {
             p1_answer += 1 * 2u32.pow(wins_count as u32 - 1);
         }
 
-        scratch_cards.push(ScratchCards::new(wins_count));
-    }
-
-    for i in 0..scratch_cards.len() {
-        let sc = &scratch_cards[i];
-        let instances_to_add = sc.instances;
-
-        for w in 0..sc.wins_count {
-            let next_card = &mut scratch_cards[i + w + 1];
-            next_card.instances += instances_to_add;
+        let current_card_instances = scratch_cards[i];
+        for w in 0..wins_count {
+            scratch_cards[i + w + 1] += current_card_instances;
         }
     }
 
-    let p2_answer = scratch_cards.iter().fold(0usize, |a, sc| a + sc.instances);
+    let p2_answer = scratch_cards.iter().fold(0usize, |a, sc| a + sc);
 
     println!("p1 answer: {p1_answer}");
     println!("p2 answer: {p2_answer}");
