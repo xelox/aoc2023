6615c1b441e60cb21928379d0fef7a6b1263db6d
xelox <alex@gorodoro.dev>
2023-12-05 21:42:19 +0200

    d5 p2

--- d5/src/main.rs
+++ d5/src/main.rs
@@ -1,27 +1,66 @@
-use std::{env::args, fs, u32};
+use std::{env::args, fs, u64, time::Instant};
 
 struct AlmanacMap {
-    dest: u32,
-    src: u32,
-    range: u32
+    dest_start: u64,
+    src_start: u64,
+    src_end: u64,
+    range: u64,
 }
 
 impl AlmanacMap {
-    fn from(list: Vec<u32>) -> AlmanacMap {
+    fn from(list: Vec<u64>) -> AlmanacMap {
         AlmanacMap { 
-            dest: list[0], 
-            src: list[1], 
-            range: list[2] 
+            dest_start: list[0], 
+            src_start: list[1], 
+            src_end: list[1] + list[2],
+            range: list[2]
         }
     }
 }
 
-fn main() {
+fn intersect_ranges(range: &(u64, u64), cutter: &(u64, u64)) -> Option<Vec<(u64, u64)>> {
+    let mut result: Vec<(u64, u64)> = Vec::new();
+
+    let (range_start, range_size) = range;
+    let range_end = range_start + range_size;
+
+    let (cutter_start, cutter_size) = cutter;
+    let cutter_end = cutter_start + cutter_size - 1;
+
+    //range inside the cutter range
+    if range_start >= cutter_start && range_end <= cutter_end {
+        return None;
+    }
+
+    //range partially overlap cutter
+    if range_start <= &cutter_end && range_end >= *cutter_start {
+
+        let a_start: u64 = *range_start;
+        let b_start: u64;
+        if cutter_start < &a_start {
+            b_start = cutter_end + 1;
+        } else {
+            b_start = *cutter_start;
+        }
+        let a_count = b_start - a_start - 1;
+        let b_count = range_size - a_count - 1;
+
+        result.push( (a_start, a_count) );
+        result.push( (b_start, b_count) );
+
+        return Some(result);
+    }
+
+    return None;
+}
+
+fn main() { 
+    // env::set_var("RUST_BACKTRACE", "1");
     let path = args().nth(1).unwrap();
     let input = fs::read_to_string(path).unwrap();
     let mut lines = input.lines();
 
-    let seeds: Vec<u32> = lines.next()
+    let seeds: Vec<u64> = lines.next()
         .unwrap()
         .split(": ")
         .nth(1)
@@ -51,19 +90,21 @@ fn main() {
             .push(almanac_map);
     }
 
-    // min location number
-    let mut p1_answer: u32 = u32::MAX;
+    // min location number p1
+    let mut p1_answer: u64 = u64::MAX;
+    let start_time = Instant::now();
 
+    //p1 solution
     //for each seed
-    for seed in seeds {
+    for seed in seeds.as_slice() {
         //we walk the map to map sequence
-        let mut current_cue = seed;
+        let mut current_cue = seed.clone();
         for map_set in maps_sets.as_slice() {
             for map in map_set {
-                let falls_into_range = current_cue >= map.src && current_cue < map.src + map.range;
+                let falls_into_range = current_cue >= map.src_start && current_cue < map.src_end;
                 if falls_into_range {
-                    let diff = current_cue - map.src;
-                    current_cue = map.dest + diff; 
+                    let diff = current_cue - map.src_start;
+                    current_cue = map.dest_start + diff; 
                     break;
                 }
             }
@@ -73,5 +114,62 @@ fn main() {
         }
     } 
 
-    println!("p1 answer: {p1_answer}");
+    let p1_elapsed = Instant::now() - start_time;
+
+    
+
+    //min location number p2
+    let mut p2_answer: u64 = u64::MAX;
+
+    //p2 solution
+    //for each seed range
+    for seeds_sr in seeds.chunks(2) {
+        let mut ranges: Vec<(u64, u64)> = Vec::new();
+        ranges.push((seeds_sr[0], seeds_sr[1]));
+        let mut next_gen_ranges: Vec<(u64, u64)> = Vec::new();
+
+        for map_set in maps_sets.as_slice() {
+            for range in ranges.as_slice() {
+                let mut found_intersection = false;
+                for map in map_set {
+                    let search_intersect = intersect_ranges(&range, &(map.src_start, map.range));
+                    match search_intersect {
+                        Some(new_ranges) => {
+                            next_gen_ranges = [next_gen_ranges, new_ranges].concat();
+                            found_intersection = true;
+                            break;
+                        } 
+                        None => {}
+                    }
+                }
+                if !found_intersection {
+                    next_gen_ranges.push(*range);
+                }
+            }
+
+            for ng in next_gen_ranges.iter_mut() {
+                for map in map_set {
+                    let falls_into_range = ng.0 >= map.src_start && ng.0 < map.src_end;
+                    if falls_into_range {
+                        let diff = ng.0 - map.src_start;
+                        ng.0 = map.dest_start + diff; 
+                        break;
+                    }
+                }
+            }
+            ranges = next_gen_ranges.clone();
+            next_gen_ranges.clear();
+        }
+
+        for (range_start, _) in ranges.as_slice() {
+            if range_start < &p2_answer {
+                p2_answer = *range_start;
+            } 
+        }
+    }
+
+    let p2_elapsed = Instant::now() - start_time;
+
+    println!("p1 answer: {p1_answer} in {:.2?}", p1_elapsed);
+    println!("p2 answer: {p2_answer} in {:.2?}", p2_elapsed);
 }
